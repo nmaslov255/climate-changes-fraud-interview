2c9b20fb6010b042c50a08effe520c0bacdb39a5
nmaslov255 <nmaslov255@yandex.ru>
2022-12-04 14:48:35 +0600

    added parse_1st_page_review

--- app/app/settings.py
+++ app/app/settings.py
@@ -6,6 +6,7 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from webdriver_manager.chrome import ChromeDriverManager
 
 BOT_NAME = 'app'
 
@@ -14,18 +15,24 @@ NEWSPIDER_MODULE = 'app.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'app (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+
+SELENIUM_DRIVER_NAME = 'chrome'
+
+SELENIUM_DRIVER_EXECUTABLE_PATH = ChromeDriverManager().install()
+SELENIUM_DRIVER_ARGUMENTS=[] # -headless
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3.5
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -50,9 +57,13 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'app.middlewares.AppDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_selenium.SeleniumMiddleware': 800
+}
+
+FEED_EXPORTERS = {
+    'xlsx': 'scrapy_xlsx.XlsxItemExporter',
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
--- app/app/spiders/interview.py
+++ app/app/spiders/interview.py
@@ -1,10 +1,32 @@
 import scrapy
 
+from scrapy_selenium import SeleniumRequest
+from scrapy.selector import Selector
+
+from time import sleep
+
+START_URL = 'https://forms.gle/s8ME2PMP9o5PMKsW7' 
 
 class InterviewSpider(scrapy.Spider):
     name = 'interview'
-    allowed_domains = ['https://forms.gle/s8ME2PMP9o5PMKsW7']
-    start_urls = ['https://forms.gle/s8ME2PMP9o5PMKsW7']
+    allowed_domains = [START_URL]
+    start_urls = [START_URL]
 
-    def parse(self, response):
+    def start_requests(self):
+        yield SeleniumRequest(url=START_URL, callback=self.parse_1st_page_review)
+
+    def parse_1st_page_review(self, responce):
+        driver = responce.meta['driver']
+
+        sleep(2)
+        driver.find_element_by_css_selector('div[data-value="English language"]').click()
+        sleep(2)
+        driver.find_element_by_css_selector('div[data-is-receipt-checked] div[role="button"]').click()
+        sleep(2)
+
+        self.parse_2nd_page_review(driver)
+
+    def parse_2nd_page_review(self, driver):
+        driver.find_element_by_css_selector('input[aria-labelledby="i1"]').send_keys('Николай')
+        import ipdb; ipdb.set_trace()
         pass
