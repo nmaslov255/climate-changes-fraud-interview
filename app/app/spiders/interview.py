da14b933bc44e6a1a8cc87bbd5e8e4d47c7b5c67
nmaslov255 <nmaslov255@yandex.ru>
2022-12-05 13:49:11 +0600

    complete other interview pages

--- app/app/spiders/interview.py
+++ app/app/spiders/interview.py
@@ -41,6 +41,7 @@ class InterviewSpider(scrapy.Spider):
         self.parse_2nd_page_review()
 
     def parse_2nd_page_review(self):
+        sleep(5)
 
         self.driver.find_element_by_css_selector('input[aria-labelledby="i1"]').send_keys(choice(CITY_KG_EN))
         sleep(2)
@@ -59,7 +60,6 @@ class InterviewSpider(scrapy.Spider):
     def parse_3th_page_review(self):
         sleep(5)
         self.click_by_label_for(5) # i5|8|11|14|17|20
-        import ipdb; ipdb.set_trace()
 
         for idx in (2, 4, 6):
             table_1_query = ( 'div[aria-labelledby="i23"] > div:nth-child(1) > '
@@ -83,8 +83,86 @@ class InterviewSpider(scrapy.Spider):
         self.parse_4th_page_review()
 
     def parse_4th_page_review(self):
-        import ipdb; ipdb.set_trace()
-        pass
+        sleep(5)
+
+        self.click_by_label_for(5) # i5|8|11|14
+        
+        sleep(1)
+        for idx in (2, 4, 6):
+            table_1_query = ( 'div[aria-labelledby="i17"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+        sleep(1)
+
+        self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
+
+        self.parse_5th_page_review()
+
+
+    def parse_5th_page_review(self):
+        sleep(5)
+
+        self.click_by_label_for(5) # i5|8|11|14
+        
+        sleep(1)
+        self.click_by_label_for(21) # i21|24
+
+        sleep(1)
+        self.click_by_label_for(31) # i31|34
+
+        sleep(1)
+        self.click_by_label_for(41) # i41|44|47|50|53
+
+        sleep(1)
+        self.click_by_label_for(60) # i60|63|66|69|72
+
+        sleep(1)
+        self.click_by_label_for(79) # i79|82|85|88
+        
+        self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
+
+        self.parse_6th_page_review()
+
+    def parse_6th_page_review(self):
+        sleep(5)
+
+        self.driver.find_element_by_css_selector('input[aria-labelledby="i1"]').send_keys(randrange(7))
+        
+        sleep(1)
+        for idx in (2, 4, 6):
+            table_1_query = ( 'div[aria-labelledby="i5"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+        sleep(1)
+        for idx in (2, 4, 6):
+            table_1_query = ( 'div[aria-labelledby="i9"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+        self.click_by_label_for(17) # i17|20
+
+        sleep(1)
+        for idx in (2, 4, 6, 8, 10, 12, 14):
+            table_1_query = ( 'div[aria-labelledby="i26"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+
+        sleep(1)
+        for idx in (2, 4, 6, 8, 10, 12, 14, 16, 18, 20):
+            table_1_query = ( 'div[aria-labelledby="i30"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+        sleep(1)
+        self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
 
     def click_by_label_for(self, id):
         try:
