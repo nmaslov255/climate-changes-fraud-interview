9d2dd67c468d04df9e98ca4864db15bfb7149068
nmaslov255 <nmaslov255@yandex.ru>
2022-12-05 13:08:11 +0600

    added 3th page interview

--- app/app/spiders/interview.py
+++ app/app/spiders/interview.py
@@ -4,7 +4,7 @@ from scrapy_selenium import SeleniumRequest
 from scrapy.selector import Selector
 
 from time import sleep
-from random import choice
+from random import choice, randrange
 
 START_URL = 'https://forms.gle/s8ME2PMP9o5PMKsW7'
 
@@ -23,34 +23,75 @@ class InterviewSpider(scrapy.Spider):
     allowed_domains = [START_URL]
     start_urls = [START_URL]
 
+    debug = True
+    driver = None
+
     def start_requests(self):
         yield SeleniumRequest(url=START_URL, callback=self.parse_1st_page_review)
 
     def parse_1st_page_review(self, responce):
-        driver = responce.meta['driver']
+        self.driver = responce.meta['driver']
 
         sleep(5)
-        driver.find_element_by_css_selector('div[data-value="English language"]').click()
+        self.driver.find_element_by_css_selector('div[data-value="English language"]').click()
         sleep(2)
-        driver.find_element_by_css_selector('div[data-is-receipt-checked] div[role="button"]').click()
+        self.driver.find_element_by_css_selector('div[data-is-receipt-checked] div[role="button"]').click()
         sleep(2)
 
-        self.parse_2nd_page_review(driver)
+        self.parse_2nd_page_review()
 
-    def parse_2nd_page_review(self, driver):
-        driver.find_element_by_css_selector('input[aria-labelledby="i1"]').send_keys(choice(CITY_KG_EN))
+    def parse_2nd_page_review(self):
+
+        self.driver.find_element_by_css_selector('input[aria-labelledby="i1"]').send_keys(choice(CITY_KG_EN))
         sleep(2)
-        driver.find_element_by_css_selector('label[for="i9"]').click() # i9|12
+        self.click_by_label_for(9) # i9|12
         sleep(2)
-        driver.find_element_by_css_selector('label[for="i22"]').click() # i22|25|28|31
+        self.click_by_label_for(22) # i22|25|28|31
         sleep(2)
-        driver.find_element_by_css_selector('label[for="i38"]').click() # i38|41|44|47|50|53|56|59
+        self.click_by_label_for(38) # i38|41|44|47|50|53|56|59
         sleep(2)
-        driver.find_element_by_css_selector('label[for="i69"]').click() # i66|69|72|75|78
+        self.click_by_label_for(69) # i66|69|72|75|78
         sleep(2)
-        driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
+        self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
         
-        self.parse_3th_page_review(driver)
+        self.parse_3th_page_review()
 
-    def parse_3th_page_review(self, driver):
+    def parse_3th_page_review(self):
+        sleep(5)
+        self.click_by_label_for(5) # i5|8|11|14|17|20
         import ipdb; ipdb.set_trace()
+
+        for idx in (2, 4, 6):
+            table_1_query = ( 'div[aria-labelledby="i23"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+        sleep(1)
+
+        for idx in (2, 4, 6):
+            table_1_query = ( 'div[aria-labelledby="i27"] > div:nth-child(1) > '
+                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+
+            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+
+        sleep(1)
+        self.click_by_label_for(35) # i35|38|41|44
+
+        self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
+
+        self.parse_4th_page_review()
+
+    def parse_4th_page_review(self):
+        import ipdb; ipdb.set_trace()
+        pass
+
+    def click_by_label_for(self, id):
+        try:
+            self.driver.find_element_by_css_selector(f'label[for="i{id}"]').click() # i9|12
+        except:
+            if self.debug == True:
+                import ipdb; ipdb.set_trace()
+            else:
+                self.logger.critical(f"""Can't find label[for="i{id}"]""")
+
