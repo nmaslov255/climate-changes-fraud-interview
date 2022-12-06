1ef67a28a4db4f6f3929bba9a7185afece58f7fc
nmaslov255 <nmaslov255@yandex.ru>
2022-12-06 13:53:06 +0600

    save answers from interview table

--- app/app/spiders/interview.py
+++ app/app/spiders/interview.py
@@ -53,18 +53,18 @@ class InterviewSpider(scrapy.Spider):
         self._input_form_for(1, 'Please indicate your city', choice(CITY_KG_EN))
         sleep(2)
 
-        self.click_by_label_for(9, 'Please indicate your gende', {9: 'Female', 12: 'Male'}) # i9|12
+        self._click_by_label_for(9, 'Please indicate your gende', {9: 'Female', 12: 'Male'}) # i9|12
         sleep(2)
-        self.click_by_label_for(22, 'Please indicate your age', 
+        self._click_by_label_for(22, 'Please indicate your age', 
             {22: '16-24', 25: '25-44', 28: '45-64', 31: 'Prefer not to say'}
         ) # i22|25|28|31
         sleep(2)
-        self.click_by_label_for(38, 'Please indicate your household income per month',
+        self._click_by_label_for(38, 'Please indicate your household income per month',
             {38: '10,000 KGS', 41: '20,000 KGS', 44: '30,000 KGS', 47: '40,000 KGS', 50: '50,000 KGS', 
              53: '75,000 KGS and more', 56: 'I don\'t know', 59: 'Prefer not to say'}
          ) # i38|41|44|47|50|53|56|59
         sleep(2)
-        self.click_by_label_for(69, 'Please indicate your education level', {
+        self._click_by_label_for(69, 'Please indicate your education level', {
             66: 'No Formal Education', 69: 'Primary Education', 72: 'Secondary', 75: 'University Degree', 
             78: 'Master or equivalent'}
         ) # i66|69|72|75|78
@@ -75,38 +75,34 @@ class InterviewSpider(scrapy.Spider):
 
     def parse_3th_page_review(self):
         sleep(5)
-        self.click_by_label_for(5,
+        self._click_by_label_for(5,
             'Do any of your friends, family or colleagues make sacrifices to protect the environment?',
             {5: 'Never', 8: 'Very rarely', 11: 'Rarely', 14: 'Sometimes', 17: 'Always', 20: 
             'I dont know we never discuss this'}
         ) # i5|8|11|14|17|20
-
-        for idx in (2, 4, 6):
-            table_1_query = ( 'div[aria-labelledby="i23"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
-
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
-
 
         self._input_table_form_for(
             23, 
             ('How much influence do your family, friends and colleagues have on your decision to make sacrifices '
              'to protect the environment?'), 
             ['Family', 'Friends', 'Colleagues'], 
-            [randrange(2, 7) for _ in range(5)],
+            [randrange(5) for _ in range(3)],
             ['Never', 'Rarely', 'From time to time', 'Often', 'Always']
         )
         sleep(1)
-        import ipdb; ipdb.set_trace()
-        for idx in (2, 4, 6):
-            table_1_query = ( 'div[aria-labelledby="i27"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
-
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
-
+        self._input_table_form_for(
+            27, 
+            ('What do you think people would think of you if you made sacrifices to protect '
+             'the environment?'), 
+            ['Family', 'Friends', 'Colleagues'], 
+            [randrange(5) for _ in range(3)],
+            ['Very Unfavourable: They try to stop me', 'Unfavourable but do not try to stop me',
+             'They don\'t care', 'Favorable: They support me but wont make sacrifices themselves', 
+             'Very favorable: They d like to also make sacrifices']
+        )
         sleep(1)
 
-        self.click_by_label_for(35, 
+        self._click_by_label_for(35, 
             ('Following environmental influencers: I follow influencers on social media (e.g. YouTube, Instagram) '
             'who post about the environment and sustainability'),
             {35: 'I never do, I am not interested', 38: 'I rarely do', 
@@ -120,17 +116,20 @@ class InterviewSpider(scrapy.Spider):
     def parse_4th_page_review(self):
         sleep(5)
 
-        self.click_by_label_for(5, 'How easy would you find it to make sacrifices to protect the environment?',
+        self._click_by_label_for(5, 'How easy would you find it to make sacrifices to protect the environment?',
             {5: 'Very Difficult', 8: 'Difficult', 11: 'Easy', 14: 'Very Easy'}
         ) # i5|8|11|14
         
         sleep(1)
-        for idx in (2, 4, 6):
-            table_1_query = ( 'div[aria-labelledby="i17"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
-
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
-
+        self._input_table_form_for(
+            17, 
+            ('I feel I could influence my workplace/boss to be more environmentally '
+             'friendly (pollute less, use electric car, use less plastic…)'), 
+            ['Workplace / Colleagues / Boss', 'Friends', 'Colleagues'], 
+            [randrange(5) for _ in range(3)],
+            ['I could NOT influence them at all', 'It is not likely I could influence them',
+             'Maybe / not sure', 'I could likely influence them', 'I could very likely influence them']
+        )
         sleep(1)
 
         self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
@@ -141,78 +140,94 @@ class InterviewSpider(scrapy.Spider):
     def parse_5th_page_review(self):
         sleep(5)
 
-        self.click_by_label_for(5, 'I believe that Air quality in my city is',
+        self._click_by_label_for(5, 'I believe that Air quality in my city is',
             {5: 'Very bad', 8: 'Bad', 11: 'Good', 14: 'Very good'}
         ) # i5|8|11|14
         
         sleep(1)
-        self.click_by_label_for(21, 'I know what PM2.5 means', 
+        self._click_by_label_for(21, 'I know what PM2.5 means', 
             {21: 'Yes', 24: 'No'}
         ) # i21|24
 
         sleep(1)
-        self.click_by_label_for(31, 'I understand the danger of air pollution',
+        self._click_by_label_for(31, 'I understand the danger of air pollution',
             {31: 'Yes', 34: 'No'}
         ) # i31|34
 
         sleep(1)
-        self.click_by_label_for(41, 'How important is the issue of climate change to you personally?', 
+        self._click_by_label_for(41, 'How important is the issue of climate change to you personally?', 
             {41: 'This is not an issue at all', 44: 'I don\'t care about this issue', 47: 'I am indifferent', 
              50: 'I somewhat care', 53: 'I very much care'}
         ) # i41|44|47|50|53
 
         sleep(1)
-        self.click_by_label_for(60, 'How much do you know about climate change', 
+        self._click_by_label_for(60, 'How much do you know about climate change', 
             {60: 'This is not an issue', 63: 'I don\'t know anything', 66: 'I am not interested to know', 
              69: 'I know a little bit', 72: 'I know a lot'}
         ) # i60|63|66|69|72
 
         sleep(1)
-        self.click_by_label_for(79, 'Climate change does not exist', 
+        self._click_by_label_for(79, 'Climate change does not exist', 
             {79: 'Climate change is caused only by natural processes', 
              82: 'Climate change is caused only by human activity', 85: 'I don’t know what is causing climate change',
              88: 'Climate change does not exist'}
         ) # i79|82|85|88
         
         self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
-        import ipdb; ipdb.set_trace()
 
         self.parse_6th_page_review()
 
     def parse_6th_page_review(self):        
         sleep(5)
 
-        self.input_form_for(1, 'Average smoking member in your family', randrange(7))
+        self._input_form_for(1, 'Average smoking member in your family', randrange(5))
         sleep(1)
-        for idx in (2, 4, 6):
-            table_1_query = ( 'div[aria-labelledby="i5"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
-
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
-
-        sleep(1)
-        for idx in (2, 4, 6):
-            table_1_query = ( 'div[aria-labelledby="i9"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
-
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
-
-        self.click_by_label_for(17, 'Do you use an air purifier at home', {17: 'Yes', 20: 'No'}) # i17|20
+        self._input_table_form_for(
+            5, 
+            ('What do you use for home heating'),
+            ['Coal', 'Gas', 'Electricity'], 
+            [randrange(4) for _ in range(3)],
+            ['Never', 'Rarely', 'From time to time', 'Always']
+        )
 
         sleep(1)
-        for idx in (2, 4, 6, 8, 10, 12, 14):
-            table_1_query = ( 'div[aria-labelledby="i26"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+        self._input_table_form_for(
+            9, 
+            ('What do you use for cooking meal'),
+            ['Coal', 'Gas', 'Electricity'], 
+            [randrange(4) for _ in range(3)],
+            ['Never', 'Rarely', 'From time to time', 'Always']
+        )
 
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
-
+        self._click_by_label_for(17, 'Do you use an air purifier at home', {17: 'Yes', 20: 'No'}) # i17|20
 
         sleep(1)
-        for idx in (2, 4, 6, 8, 10, 12, 14, 16, 18, 20):
-            table_1_query = ( 'div[aria-labelledby="i30"] > div:nth-child(1) > '
-                             f'div > div:nth-child({idx}) > span > div:nth-child({randrange(2, 6)}) > div > div' )
+        self._input_table_form_for(
+            26, 
+            ('Have you experienced the following in the last three months?'),
+            ['Cough', 'Dry Throat', 'Sneezing/allergy', 'Flu', 'Asthma', 'Fever', 'Other'], 
+            [randrange(5) for _ in range(7)],
+            ['Never', 'Very Rarely', 'Often', 'Nearly Always', 'Always']
+        )
 
-            table_1 = self.driver.find_element_by_css_selector(table_1_query).click()
+        sleep(1)
+
+        self._input_table_form_for(
+            30, 
+            ('Pro-Environmental Behaviour / Intentions'),
+            ['I leave the water running while I brush my teeth', 
+             'I forget to turn off the light when I leave my room', 
+             'I leave the fridge door open while I think about what I go eat', 
+             'Outside I put my trash in the bin',
+             'I leave the TV on while I\'m doing other things in the house', 
+             'I read documents or books about environmental protection', 
+             'I am willing to change my behaviour to change the state of the environment',
+             'I would support those who care about the environment',
+             'I believe that more trees should be planted',
+             'I try to push my company/work to be more green'], 
+            [randrange(5) for _ in range(10)],
+            ['Never', 'Very rarely', 'Sometime', 'Often', 'Always']
+        )
 
         sleep(1)
         # self.driver.find_elements_by_css_selector('div[data-is-receipt-checked] div[role="button"]')[1].click()
@@ -225,11 +240,11 @@ class InterviewSpider(scrapy.Spider):
             for idx, question in enumerate(questions):
                 answer_id = (idx+1)*2
                 table_query = (f'div[aria-labelledby="i{id}"] > div:nth-child(1) > div > '
-                               f'div:nth-child({answer_id}) > span > div:nth-child({answer_options[idx]}) > '
+                               f'div:nth-child({answer_id}) > span > div:nth-child({answer_options[idx]+2}) > '
                                 'div > div')
 
                 self.driver.find_element_by_css_selector(table_query).click()
-                answer += f'{question}: {answer_text[idx]}\n'
+                answer += f'{question}: {answer_text[answer_options[idx]]}\n'
 
             self.answers.append(answer)
         except:
